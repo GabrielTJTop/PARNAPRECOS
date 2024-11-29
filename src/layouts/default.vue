b2cdb50a2d8e0febf2fa3242b665d24b124a8b0c
Gabriel Pereira <snippighost1233@gmail.com>
2024-11-29 18:11:35 -0300

    Pequenas mudanças no index.vue

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-main>
-      <v-app-bar>
+      <v-app-bar flat>
           <v-app-bar-nav-icon scroll-behavior="collapse" scroll-threshold="190" ></v-app-bar-nav-icon>
         <v-app-bar-title style="font-family: Cochin; font-size: 22px;">
           PARNAPREÇOS
@@ -9,6 +9,7 @@
         
         <v-icon icon="mdi-magnify" color="info"></v-icon>
       </v-app-bar>
+      <v-navigation-drawer></v-navigation-drawer>
 
       <router-view />
     </v-main>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,28 @@
 <template>
-  Página principal
+  <v-container grid-list-xs>
+    <div class="ma-4 ga-1 pl-4 pt-4 d-flex ">
+      <v-row justify="space-around">
+        <div style="width: 150px; height: 150px;" class="border rounded" >
+          oi
+        </div>
+        <div style="width: 150px; height: 150px;" class="border rounded" >
+          oi
+        </div>
+        <div style="width: 150px; height: 150px;" class="border rounded" >
+          oi
+        </div>
+        <div style="width: 150px; height: 150px;" class="border rounded" >
+          oi
+        </div>
+        <div style="width: 150px; height: 150px;" class="border rounded" >
+          oi
+        </div>
+
+      </v-row>
+                                                   
+    </div>
+    
+  </v-container>
 </template>
 
 <script setup>
