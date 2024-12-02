d5e972f68cf59d39249eb59e2be44382bf8535e6
Gabriel Pereira <snippighost1233@gmail.com>
2024-12-02 18:05:49 -0300

    adicionando o Supabase

--- src/pages/item/adicionar.vue
+++ src/pages/item/adicionar.vue
@@ -1,7 +1,51 @@
 <template>
- Adicionar item
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="firstname"
+            :counter="10"
+            label="First name"
+          ></v-text-field>
+        </v-col>
+
+        <!-- <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="lastname"
+            :counter="10"
+            :rules="nameRules"
+            label="Last name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+        </v-col> -->
+      </v-row>
+    </v-container>
+  </v-form>
 </template>
-  
+
 <script setup>
- //
+import { onMounted, ref } from "vue";
+const firstname = ref()
+console.log (firstname.value)
+onMounted(() => {
+})
 </script>
