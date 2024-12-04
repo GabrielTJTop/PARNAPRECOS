3f6e3e50bb2c3062a702612c3331493960d32d8e
GabrielTJTop <168040986+GabrielTJTop@users.noreply.github.com>
2024-12-04 17:03:34 -0300

    Update default.vue

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,1 +1,51 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-app-bar scroll-behavior="collapse" scroll-threshold="121" flat>
+        
+        <v-app-bar-title style="font-family: Cochin; font-size: 22px;">
+          <v-icon icon="mdi-magnify" style="font-size: 18px;"></v-icon>
+          PARNAPREÇOS
+        </v-app-bar-title>
+        <v-icon icon="mdi-magnify" color="info"></v-icon>
+      </v-app-bar>
+  
+      <v-navigation-drawer expand-on-hover rail>
+        <v-list-item
+          prepend-icon="mdi-home"
+          title="Home"
+          :to="'/'"
+        ></v-list-item>
+        <v-list density="compact">
+          <v-list-group value="produtos">
+            <template v-slot:activator="{ props }">
+              <v-list-item v-bind="props" title="Produtos" prepend-icon="mdi-store"></v-list-item>
+            </template>
+            <v-list-item
+              prepend-icon="mdi-plus"
+              :to="'/products/adicionar'"
+            >
+            adicionar produtos
+            </v-list-item>
+            <v-list-item 
+              v-for="(categoria, i) in categorias"
+              :key="i"
+              :title="categoria.title"
+              :value="categoria.title"
+              :to="categoria.to"
+            >      
+            </v-list-item>
+          </v-list-group>
+        </v-list>
+      </v-navigation-drawer>
+    <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
 
+<script setup>
+ const categorias = [
+   { title: 'Eletrônicos', icon: '', to: '/categorias/eletronicos' },
+   { title: 'eletrodomesticos', icon: '', to: '/categorias/eletrodomesticos' }
+ ]
+</script>
