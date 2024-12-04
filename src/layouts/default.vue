1e69c5c1d8d40098fe60e591b2ab96154591feea
Gabriel Gomes <snippighost1233@gmail.com>
2024-12-04 10:20:34 -0300

    Layout funcionando

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,45 +1,39 @@
 <template>
   <v-app>
-    <v-app-bar scroll-behavior="collapse" scroll-threshold="121" flat>
-      
-      <v-app-bar-title style="font-family: Cochin; font-size: 22px;">
-        <v-icon icon="mdi-magnify" style="font-size: 18px;"></v-icon>
-        PARNAPREÇOS
-      </v-app-bar-title>
-
-      <v-icon icon="mdi-magnify" color="info"></v-icon>
-    </v-app-bar>
-
-    <v-navigation-drawer expand-on-hover rail>
-      <v-list-item
-        prepend-icon="mdi-home"
-        title="Home"
-        :to="'/'"
-      ></v-list-item>
-      <v-list density="compact">
-        <v-list-group value="produtos">
-          <template v-slot:activator="{ props }">
-            <v-list-item v-bind="props" title="Produtos" prepend-icon="mdi-store"></v-list-item>
-            <v-list-group value="categorias">
-              <template v-slot:activator="{ props }">
-                
-              </template>
-              <v-list-item
-                v-for="(categoria, i) in categorias"
-                :key="i"
-                :value="categoria"
-                color="primary"
-                :to="categoria.to"
-              >
-                <v-list-item-title>{{ categoria.title }}</v-list-item-title>
-              </v-list-item>
-            </v-list-group>
-          </template>
-
-        </v-list-group>
-      </v-list>
-    </v-navigation-drawer>
     <v-main>
+      <v-app-bar scroll-behavior="collapse" scroll-threshold="121" flat>
+        
+        <v-app-bar-title style="font-family: Cochin; font-size: 22px;">
+          <v-icon icon="mdi-magnify" style="font-size: 18px;"></v-icon>
+          PARNAPREÇOS
+        </v-app-bar-title>
+  
+        <v-icon icon="mdi-magnify" color="info"></v-icon>
+      </v-app-bar>
+  
+      <v-navigation-drawer expand-on-hover rail>
+        <v-list-item
+          prepend-icon="mdi-home"
+          title="Home"
+          :to="'/'"
+        ></v-list-item>
+        <v-list density="compact">
+          <v-list-group value="produtos">
+            <template v-slot:activator="{ props }">
+              <v-list-item v-bind="props" title="Produtos" prepend-icon="mdi-store"></v-list-item>
+            </template>
+            <v-list-item 
+              v-for="(categoria, i) in categorias"
+              :key="i"
+              :title="categoria.title"
+              :value="categoria.title"
+              :to="categoria.to"
+            >
+              
+            </v-list-item>
+          </v-list-group>
+        </v-list>
+      </v-navigation-drawer>
     <router-view></router-view>
     </v-main>
   </v-app>
@@ -48,6 +42,6 @@
 <script setup>
  const categorias = [
    { title: 'Eletrônicos', icon: '', to: '/categorias/eletronicos' },
-   { title: 'roupas', icon: '', to: '/categorias/roupas' }
+   { title: 'eletrodomesticos', icon: '', to: '/categorias/eletrodomesticos' }
  ]
 </script>
